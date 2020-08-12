ddb842eafac07339246d0b2260e99b0c35ce16c6
Pavlo-Hnatenko <gnatenko.pavlo@gmail.com>
2020-08-12 11:36:56 +0300

    third commit: class AppProperties was added

---
+++ src/main/java/AppProperties.java
@@ -0,0 +1,17 @@
+public class AppProperties {
+
+    public int id;
+
+    @PropertyKey(Key.PROPERTY_NAME)
+    public String name;
+
+    @PropertyKey(Key.PROPERTY_UTILITY)
+    public boolean utility;
+
+    @PropertyKey(Key.PROPERTY_PRICE)
+    public int price;
+
+    public long someLong;
+
+}
+
